1b2b127c0de264771e01024efddc9ff3782511c8
heihong-dgfip <hei-hong.traing@dgfip.finances.gouv.fr>
2022-07-18 16:03:56 +0200

    init

---
+++ packages/core/stencil.config.ts
@@ -0,0 +1,22 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'core',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+      dir: 'components',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
