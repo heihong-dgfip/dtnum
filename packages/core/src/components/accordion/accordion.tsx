bb86dc1148bc874cd1db9324ba75e513b72b45fc
heihong-dgfip <hei-hong.traing@dgfip.finances.gouv.fr>
2022-07-20 16:30:54 +0200

    demo dynamique

--- packages/core/src/components/accordion/accordion.tsx
+++ packages/core/src/components/accordion/accordion.tsx
@@ -11,11 +11,11 @@ export class Accordion {
       <section class="fr-accordion">
         <h3 class="fr-accordion__title">
           <button class="fr-accordion__btn" aria-expanded="false" aria-controls="accordion1">
-            titre
+            <slot name="title"></slot>
           </button>
         </h3>
         <div id="accordion1" class="fr-collapse">
-          description
+          <slot />
         </div>
       </section>
     );
