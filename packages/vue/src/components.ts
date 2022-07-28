bec26a0c3befbca308b23a7fcab36d7a0c754fc2
heihong-dgfip <hei-hong.traing@dgfip.finances.gouv.fr>
2022-07-28 16:23:08 +0200

    header

--- packages/vue/src/components.ts
+++ packages/vue/src/components.ts
@@ -6,7 +6,11 @@ import { defineContainer } from './vue-component-lib/utils';
 import type { JSX } from '@dtnum/core/components';
 
 import { defineCustomElement as defineFrAccordion } from '@dtnum/core/components/fr-accordion.js';
+import { defineCustomElement as defineFrHeader } from '@dtnum/core/components/fr-header.js';
 
 
 export const FrAccordion = /*@__PURE__*/ defineContainer<JSX.FrAccordion>('fr-accordion', defineFrAccordion);
+
+
+export const FrHeader = /*@__PURE__*/ defineContainer<JSX.FrHeader>('fr-header', defineFrHeader);
 
