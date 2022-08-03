85ae2106517aab5b1523cc8d44df6a4dd5db1b36
heihong-dgfip <hei-hong.traing@dgfip.finances.gouv.fr>
2022-08-03 17:16:50 +0200

    conf

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -17,7 +17,7 @@ export const config: Config = {
       copy: [
         { src: 'components/**/*.html' },
         { src: './../node_modules/@gouvfr/dsfr/dist/utility', dest: 'utility' },
-        { src: './../node_modules/@gouvfr/dsfr/dist/icons', dest: 'icons' },
+        { src: './../node_modules/@gouvfr/dsfr/dist/icons', dest: 'components/icons' },
         { src: './../node_modules/@gouvfr/dsfr/dist/fonts', dest: 'fonts' },
       ],
     },
