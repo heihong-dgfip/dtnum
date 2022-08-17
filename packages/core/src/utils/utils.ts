e2b9d838137240e168ade13c6498c4832efa0d2d
heihong-dgfip <hei-hong.traing@dgfip.finances.gouv.fr>
2022-08-17 10:49:09 +0200

    test

--- packages/core/src/utils/utils.ts
+++ packages/core/src/utils/utils.ts
@@ -2,7 +2,7 @@ export function format(first: string, middle: string, last: string): string {
   return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
 }
 
-export function addElement(element) {
+function addElement(element) {
   let wrapper = document.createElement('li');
   wrapper.className = 'fr-nav__item';
   wrapper.appendChild(element);
@@ -10,3 +10,7 @@ export function addElement(element) {
 
   this.el.querySelector('ul.fr-nav__list').appendChild(wrapper);
 }
+
+module.exports = {
+  addElement,
+};
