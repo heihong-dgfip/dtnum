cd5fafd95ea81b29552a5342bb242ca9c6a876ce
heihong-dgfip <hei-hong.traing@dgfip.finances.gouv.fr>
2022-08-19 14:33:25 +0200

    update

---
+++ packages/core/src/components/select/select.tsx
@@ -0,0 +1,20 @@
+import { Component, h, Element, ComponentInterface, Method } from '@stencil/core';
+
+@Component({
+  tag: 'fr-select',
+  shadow: false,
+})
+export class Select implements ComponentInterface {
+  render() {
+    return (
+      <div class="fr-select-group">
+        <label class="fr-label" htmlFor="select">
+          Label pour liste déroulante
+        </label>
+        <select class="fr-select" id="select" name="select">
+          <slot></slot>
+        </select>
+      </div>
+    );
+  }
+}
