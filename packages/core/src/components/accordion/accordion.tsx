ca792507fb77703682c5092fa5102e2c24f9b304
heihong-dgfip <hei-hong.traing@dgfip.finances.gouv.fr>
2022-08-23 15:04:29 +0200

    add

--- packages/core/src/components/accordion/accordion.tsx
+++ packages/core/src/components/accordion/accordion.tsx
@@ -16,21 +16,32 @@ export class Accordion {
 
   slotElements: Array<any> = [];
   slotElementsOld: Array<any> = [];
-  attributes = this.el.getAttributeNames();
+  attributes = [];
   @Prop({ reflect: true }) toto = 'test';
 
   subscriberCallback(mutations) {
+    let that = this;
     mutations.forEach((mutation) => {
       if (mutation.type === 'childList') {
-        mutation.addedNodes.forEach((node) => console.log('add', node));
-        mutation.removedNodes.forEach((node) => console.log('remove', node));
+        mutation.addedNodes.forEach((node) => {
+          console.log('add', node);
+          that.slotElements.push(node);
+        });
+        mutation.removedNodes.forEach((node) => {
+          console.log(that.slotElements.indexOf(node));
+          that.slotElements.splice(that.slotElements.indexOf(node), 1);
+          console.log('remove', node);
+          console.log(that.slotElements);
+        });
       } else if (mutation.type === 'attributes') {
+        this.attributes = that.el.getAttributeNames();
         console.log(`The ${mutation.attributeName} attribute was modified.`);
       }
     });
   }
 
   componentDidLoad() {
+    console.log(window.dsfr);
     const target = this.el;
 
     const config = {
@@ -43,15 +54,17 @@ export class Accordion {
     observer.observe(target, config);
   }
 
-  slotChange(node) {
-    console.log(node);
-    this.slotElements = [...this.el.shadowRoot.querySelector('slot')?.assignedElements({ flatten: true })];
+  slotChange() {
+    if (this.slotElements.length === 0) {
+      this.slotElements = [...this.el.shadowRoot.querySelector('slot')?.assignedElements({ flatten: true })];
+      console.log(this.slotElements);
+    }
   }
 
   render() {
     return (
       <ol>
-        <slot onSlotchange={(node) => this.slotChange(node)}></slot>
+        <slot onSlotchange={() => this.slotChange()}></slot>
       </ol>
     );
   }
